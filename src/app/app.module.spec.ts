a6a214a46e2e96f6ff40aec23b28513bda42e660
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2021-11-15 11:56:39 +0700

    Added unit testing angular module specs.

--- src/app/app.module.spec.ts
+++ src/app/app.module.spec.ts
@@ -1,3 +1,41 @@
-describe('4. Angular module test scenario', () => {
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
+describe('4. Angular module test scenario', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeAll(() => {
+    TestBed.configureTestingModule({
+      imports: [ AppModule ]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
+  describe('4.1. AppModule initialization', () => {
+    it('should be initialized', () => {
+      const module: AppModule = TestBed.inject(AppModule);
+
+      expect(module).toBeTruthy();
+      expect(module).toBeInstanceOf(AppModule);
+    });
+  });
+
+  describe('4.2. AppComponent initialization', () => {
+    it('should be initialized', () => {
+      const component: AppComponent = fixture.componentInstance;
+
+      expect(component).toBeTruthy();
+      expect(component).toBeInstanceOf(AppComponent);
+    });
+  });
+
+  describe('4.3. AppComponent template view', () => {
+    it('should have router outlet', () => {
+      const element: HTMLElement = fixture.nativeElement;
+
+      expect(element.querySelector('router-outlet')).toBeTruthy();
+    });
+  });
 });
