cf58379f15cc58ccfdae13f4d26d133c29f20958
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2021-11-16 11:23:32 +0700

    Updated karma runner to use karma parallel framework.

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'/**, 'parallel' */],
     plugins: [
       require('karma-jasmine'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      // require('karma-parallel'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -25,6 +26,18 @@ module.exports = function (config) {
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
     },
+    // parallelOptions: {
+    //   executors: 10, // Defaults to cpu-count - 1
+    //   shardStrategy: 'round-robin'
+    //   // shardStrategy: 'description-length'
+    //   // shardStrategy: 'custom'
+    //   // customShardStrategy: function(config) {
+    //   //   config.executors // number, the executors set above
+    //   //   config.shardIndex // number, the specific index for the shard currently running
+    //   //   config.description // string, the name of the top-level describe string. Useful //     for determining how to shard the current specs
+    //   //   return config.
+    //   // }
+    // },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
